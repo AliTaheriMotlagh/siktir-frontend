1993929b589f3af2ed1e58db73656539a3fa703f
Ali Taheri <alitaherimotlagh@gmail.com>
2023-01-11 22:51:11 +0330

    go on render

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://siktir-backend.herokuapp.com',
+  apiUrl: 'https://siktir-backend.onrender.com',
   fpApiKey: 'iiuIAXZValPDjn7qAtXg',
   fpEndpoint: 'https://fp.siktir.fun',
 };
